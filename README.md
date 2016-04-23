ce8b6bdc0fb715d236145fd115fed2ed4cd50646
xulu <gordonzu@outlook.com>
2016-04-23 09:10:30 -0400

    update README.md

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # test-app
 
-This is my tutorial project for MEAN stack TDD
+This is my tutorial project for MEAN stack TDD with deployment to Heroku. This project is 
+following Michael Hartl's Rails 4 version 2 eBook.
 
 This project is generated with [yo angular generator](https://github.com/yeoman/generator-angular)
 version 0.15.1.
--- web.js
+++ web.js
@@ -1,10 +1,46 @@
-var gzippo = require('gzippo');
-var express = require('express');
-var morgan = require('morgan');
+'use strict';
+
+var gzippo 					= require('gzippo');
+var express 				= require('express');
+var morgan 					= require('morgan');
+var mongoose        = require('mongoose');
+var bodyParser      = require('body-parser');
+var methodOverride  = require('method-override');
+
 var app = express();
 
-app.use(morgan('dev'));
-app.use(gzippo.staticGzip("" + __dirname + "/dist"));
-app.listen(process.env.PORT || 5000);
+// add middleware needed for REST API's
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(methodOverride('X-HTTP-Method-Override'));
+
+// CORS Support
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+
+// connect to the MongoDb
+mongoose.connect('mongodb://localhost:27017/sampleapp');
+mongoose.connection.once('open', function () {
+
+// connect to data model
+// load the routes
+
+});
+
+app.use(express.static(__dirname + "/app"));
+app.use(bodyParser.json());
+
+
+console.log('listening on port 3000...');
+app.listen(3000);
+
+//app.use(morgan('dev'));
+//app.use(gzippo.staticGzip("" + __dirname + "/dist"));
+//app.listen(process.env.PORT || 5000);
 
 
